fe6863b1e30dc92b369486270135cd41233ba0b5
BrunnoBernardes <brunnobernardes@discente.ufg.br>
2025-03-07 13:36:54 -0300

    Estudo em Casa 03.07

--- aed1-codes_aulas/03.07/ex10.c
+++ aed1-codes_aulas/03.07/ex10.c
@@ -8,6 +8,9 @@ typedef struct {
 } Funcionario;
 
 void pode_aposentar(Funcionario funcionario) {
+
+    printf("\n");
+
     if (funcionario.anos_trabalhados >= 30) {
         printf("O funcionario %s (%d) pode aposentar!\n", funcionario.nome, funcionario.matricula);
     } else {
@@ -17,22 +20,28 @@ void pode_aposentar(Funcionario funcionario) {
 
 int main() {
 
-    Funcionario funcionario;
     int numero_funcionarios;
     int i;
 
     printf("Digite o numero de funcionarios: ");
     scanf("%d", &numero_funcionarios);
 
+    Funcionario funcionario[numero_funcionarios];
+
     for (i = 0; i < numero_funcionarios; i++) {
+        printf("\n");
         printf("Digite o nome, matricula, anos trabalhados e departamento do funcionario:\n");
-        scanf("%*c%[^\n] %d %d %s", funcionario.nome, &funcionario.matricula, &funcionario.anos_trabalhados, funcionario.departamento);
-        printf("Nome: %s\nMatricula: %d\nAnos trabalhados: %d\nDepartamento: %s\n", funcionario.nome, funcionario.matricula, funcionario.anos_trabalhados, funcionario.departamento);
+        getchar();
+        scanf("%[^\n] %d %d %s", funcionario[i].nome, &funcionario[i].matricula, &funcionario[i].anos_trabalhados, funcionario[i].departamento);
+        printf("\n");
+        printf("Nome: %s\nMatricula: %d\nAnos trabalhados: %d\nDepartamento: %s\n", funcionario[i].nome, funcionario[i].matricula, funcionario[i].anos_trabalhados, funcionario[i].departamento);
     }
 
     for (i = 0; i < numero_funcionarios; i++) {
         pode_aposentar(funcionario[i]);
     }
+
+    printf("\n");
 
     return 0;
 }
