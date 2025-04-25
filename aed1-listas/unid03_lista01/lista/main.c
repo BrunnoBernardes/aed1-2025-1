db2f9eb274b2d5fd9edf8b1ea568346f219ba1e3
BrunnoBernardes <brunnobernardes@discente.ufg.br>
2025-04-25 20:07:42 -0300

    Estudos 04.25

---
+++ aed1-listas/unid03_lista01/lista/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "lista.h"
+
+int main() {
+    Lista* l = criaLista();
+    if (l == NULL) {
+        printf("Erro ao criar a lista!\n");
+        return 1;
+    }
+
+    if (listaVazia(l)) printf("Lista vazia.\n");
+    else printf("Lista nao esta vazia.\n");
+
+    insereInicio(l, 20);
+    insereFim(l, 50);
+    insereOrdenado(l, 30);
+    insereFim(l, 70);
+    insereInicio(l, 10);
+    imprimeLista(l);
+
+    int valor;
+    if (consultaIndice(l, 2, &valor)) {
+        printf("Elemento no indice 2: %d\n", valor);
+    }
+
+    if (consultaValor(l, 50, &valor)) {
+        printf("Elemento 50 encontrado no indice: %d\n", valor);
+    }
+
+    if (listaCheia(l)) printf("Lista cheia.\n");
+    else printf("Lista nao esta cheia.\n");
+
+    removeInicio(l);
+    removeFim(l);
+    removeElemento(l, 30);
+
+    imprimeLista(l);
+
+    printf("Tamanho atual da lista: %d\n", tamanhoLista(l));
+
+    if (listaVazia(l)) printf("Lista vazia.\n");
+    else printf("Lista nao esta vazia.\n");
+
+    if (listaCheia(l)) printf("Lista cheia.\n");
+    else printf("Lista nao esta cheia.\n");
+
+    liberaLista(l);
+    return 0;
+}
